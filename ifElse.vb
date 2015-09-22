5c4ce090cf02807736a25801d2105dd18473e7d7
Joey Yonezawa <josephyonezawa430@Rusdlearns.net>
2015-09-22 14:40:01 -0700

    Create ifElse.vb

---
+++ ifElse.vb
@@ -0,0 +1,35 @@
+Module Module1
+
+    Sub Main()
+
+        'Console.WriteLine("Please type something and press Enter")
+
+        'Dim userValue As String
+
+        'userValue(-Console.ReadLine())
+        'Console.WriteLine("You typed: " & userValue)
+        'Console.Readline()
+
+        Console.WriteLine("Would you prefer what is behind door number 1,2, or 3?")
+        Dim userValue As String = Console.ReadLine()
+
+        If userValue = "1" Then
+            Console.WriteLine("You won a new car! ")
+            Console.ReadLine()
+        ElseIf userValue = "2" Then
+            Console.WriteLine("You won a new boat! ")
+            Console.ReadLine()
+        ElseIf userValue = "3" Then
+            Console.WriteLine("You won a Cat! ")
+            Console.ReadLine()
+        ElseIf userValue = "3" Then
+            Console.WriteLine("You won a Cat! ")
+            Console.ReadLine()
+        Else
+            Console.WriteLine("Sorry we did not understand your response.  You Lose! ")
+            Console.ReadLine()
+        End If
+
+    End Sub
+
+End Module
