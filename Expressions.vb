ea0d5bc0c4da87490462c97c09e48ef1082b4d50
Joey Yonezawa <josephyonezawa430@Rusdlearns.net>
2015-09-23 13:56:30 -0700

    Create Expressions.vb

---
+++ Expressions.vb
@@ -0,0 +1,58 @@
+ Module Module1
+
+        Sub Main()
+
+            Dim x = 0
+            Dim y = 0
+            Dim a = 0
+            Dim b = 0
+
+
+            ' Assignment operator
+            x = 3
+
+            ' Addition operator
+            x = 3 + 4
+
+            ' Subtraction operator
+            x = 4 - 3
+
+            'Multiplication operator
+            x = 10 * 5
+
+            'Division operator
+            x = 10 / 5
+
+            'Equality operator
+            If x = y Then
+                'Stuff goes here
+            End If
+
+            ' Greater than operator
+            If x > y Then
+            End If
+
+            ' Less than or equal to operator
+            If x < -y Then
+                ' Stuff goes here
+            End If
+
+            'Condition OR operator
+
+            If x > y Or a > b Then
+                ' Stuff goes here
+            End If
+
+            'Condition AND operator
+
+            If x > y And a > b Then
+                ' Stuff goes here
+            End If
+
+            ' member access and Method invocation
+            Console.WriteLine("Hi!")
+
+
+        End Sub
+
+    End Module
