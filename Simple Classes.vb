59f55d4cbf6fa105667b69fbe93aa699486f0d93
Joey Yonezawa <josephyonezawa430@Rusdlearns.net>
2015-09-25 14:48:11 -0700

    Update Simple Classes.vb

--- Simple Classes.vb
+++ Simple Classes.vb
@@ -4,13 +4,20 @@ Module Module1
 
         'Dim myNewCar As Car = New Car
         Dim myNewCar As New Car
+        'Dim myOtherCar As Car = myNewCar
+
+        'Dim myOtherCar As New Car("BMW", 2005, 30000)
+
+
+        Carlot.AddToInventory(myNewCar)
+
 
         myNewCar.Make = "Oldsmobile"
         myNewCar.Model = "Cutlas Supreme"
         myNewCar.Year = 1986
         myNewCar.Color = "Silver"
 
-        'With myNewCar
+        'With myNewCar 
         '    .Make = "Olds"
         '    .Model = "Cutlas"
         '    .Year = 1986
@@ -18,10 +25,15 @@ Module Module1
         'End With
 
         'Console.WriteLine("{0} - {1} - {2}", myNewCar.Make, myNewCar.Model, myNewCar.Year.ToString())
-
         'Console.WriteLine(determineMarketValue(myNewCar))
+        'Console.WriteLine(myNewCar.determineMarketValue)
 
-        Console.WriteLine(myNewCar.determineMarketValue)
+        Console.WriteLine("Before: " & myNewCar.Make)
+        doByValue(myNewCar)
+        Console.WriteLine("After By Value: " & myNewCar.Make)
+
+        doByReference(myNewCar)
+        Console.WriteLine("After By Reference: " & myNewCar.Make)
 
         Console.ReadLine()
 
@@ -35,21 +47,65 @@ Module Module1
 
     'End Function
 
+    Sub doByValue(ByVal myCar As Car)
+        myCar = New Car
+        myCar.Make = "BMW"
+    End Sub
+
+    Sub doByReference(ByRef myCar As Car)
+        myCar = New Car
+        myCar.Make = "BMW"
+    End Sub
+
 End Module
+
 
 Public Class Car
 
     Public Make As String
     Public Model As String
-    Public Year As String
+    Public Year As Integer
     Public Color As String
+    Public Amount As Double
+    Public Surcharge As Double
 
-    Function determineMarketValue() As Double
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal _Make As String, ByVal _Year As Integer, ByVal _amount As Double 
+        Me.Make = _Make
+        Me.Year = _Year
+        Me.Amount = _amount
+        Me.Surcharge = _amount * 1.025
+    End Sub
+
+
+    Public Function determineMarketValue() As Double
         ' Super Secret Formula
 
         Return 100.0
 
     End Function
 
+    Public Sub myMethod(ByVal myParameter As String)
+
+    End Sub
+    Public Sub myMethod(ByVal aotherversion As String, ByVal myParameter As String)
+
+    End Sub
+
+    Public Sub myMethod(ByVal myParameter As Integer)
+
+    End Sub
+
+
+End Class
+
+Public Class Carlot
+
+    Public Shared Sub AddToInventory(ByVal newCar As Car)
+        'Body of the method goes here.
+    End Sub
 
 End Class
